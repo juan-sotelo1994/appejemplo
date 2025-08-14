df5d07f1ab244181b6212a9f1a05b03bb4aaa7b1
Juan David Sotelo <131626247+juan-sotelo1994@users.noreply.github.com>
2025-08-13 20:11:02 -0500

    version8

--- lib/widgets/menuprincipal/menuprincipal.dart
+++ lib/widgets/menuprincipal/menuprincipal.dart
@@ -1,29 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:circular_menu/circular_menu.dart';
 
-@override
-Widget build(BuildContext context) {
-  return Menuprincipal();
+void main() {
+  runApp(const MyApp());
 }
 
-class Menuprincipal extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Menuprincipal(),
+    );
+  }
+}
+
+class Menuprincipal extends StatefulWidget {
   const Menuprincipal({super.key});
+
+  @override
+  State<Menuprincipal> createState() => _MenuprincipalState();
+}
+
+class _MenuprincipalState extends State<Menuprincipal> {
+  Color _color = const Color.fromARGB(255, 255, 255, 255);
+  String _colorName = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/animacion/lego.png', width: 80.0),
-            const Text(
-              'Welcome to the LEGO App',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 55.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 0, 191, 109),
+        centerTitle: true,
+        title: const Text('Menu'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          bottom: 100,
+        ), // Subir 50px, ajusta según necesites
+        child: CircularMenu(
+          alignment: Alignment.bottomCenter,
+          backgroundWidget: Center(
+            child: RichText(
+              text: TextSpan(
+                style: const TextStyle(color: Colors.black, fontSize: 28),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: _colorName,
+                    style: TextStyle(
+                      color: _color,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const TextSpan(text: ' button is clicked.'),
+                ],
               ),
+            ),
+          ),
+          toggleButtonColor: const Color.fromARGB(255, 0, 191, 109),
+          items: [
+            CircularMenuItem(
+              icon: Icons.home,
+              color: Colors.green,
+              onTap: () {
+                setState(() {
+                  _color = Colors.green;
+                  _colorName = 'Green';
+                });
+              },
+            ),
+            CircularMenuItem(
+              icon: Icons.search,
+              color: Colors.blue,
+              onTap: () {
+                setState(() {
+                  _color = Colors.blue;
+                  _colorName = 'Blue';
+                });
+              },
+            ),
+            CircularMenuItem(
+              icon: Icons.settings,
+              color: Colors.orange,
+              onTap: () {
+                setState(() {
+                  _color = Colors.orange;
+                  _colorName = 'Orange';
+                });
+              },
+            ),
+            CircularMenuItem(
+              icon: Icons.chat,
+              color: Colors.purple,
+              onTap: () {
+                setState(() {
+                  _color = Colors.purple;
+                  _colorName = 'Purple';
+                });
+              },
+            ),
+            CircularMenuItem(
+              icon: Icons.notifications,
+              color: Colors.brown,
+              onTap: () {
+                setState(() {
+                  _color = Colors.brown;
+                  _colorName = 'Brown';
+                });
+              },
             ),
           ],
         ),
